cf9b040dd3a37ae197e851aed3c6605c8b806aac
Noud <noudjanssen123@gmail.com>
2024-05-22 14:49:22 +0200

    remvod automatic signup on create

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,6 @@ import './css/core.css'
 
 import user from './controller/User.js'
 
-await user.signUp("ROC", "ifno@roc-nijmegen.nl", "Welkom123", "school");
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
