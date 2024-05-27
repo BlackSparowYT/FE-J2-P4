8b62190c0014edfb8a2b8de3723de03c5491d6e6
Jordi Verbeek <jordi.verbeek@gmail.com>
2024-05-27 17:28:32 +0200

    end of school

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./components/sidebar.jsx";
 // import Footer from './components/footer.jsx';
 import Home from "./pages/home.jsx";
 import Error404 from "./pages/404.jsx";
 import Login from "./pages/auth/login.jsx";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 function App() {
-
-    let showSidebar = true;
-
+  const [showSidebar, setShowSidebar] = useState(true);
+  const location = useLocation();
+  useEffect(() => {
     const url = window.location.href;
-    if(url.includes("account")) {
-        showSidebar = false;
+    if (url.includes("account")) {
+      setShowSidebar(false);
+    } else {
+      setShowSidebar(true);
     }
-  
+  }, [location]);
+
+  console.log(showSidebar);
+
   return (
-    <BrowserRouter>
-        {
-            showSidebar ? <Header /> : null
-        }
-        <Routes>
-            <Route path="/" Component={Home} />
-            <Route path="/404" Component={Error404} />
-            <Route path="*" Component={Error404} />
-
-            <Route path="/account/login" Component={Login} />
-
-        </Routes>
-    </BrowserRouter>
+    <>
+      {showSidebar ? <Header /> : null}
+      <Routes>
+        <Route path="/" Component={Home} />
+        <Route path="/404" Component={Error404} />
+        <Route path="*" Component={Error404} />
+        <Route path="/account/login" Component={Login} />
+      </Routes>
+    </>
   );
 }
 
--- src/pages/auth/login.jsx
+++ src/pages/auth/login.jsx
@@ -1,8 +1,77 @@
 import * as React from "react";
+import { useState } from "react";
 import Card from "@mui/joy/Card";
+import Button from "@mui/joy/Button";
+import Input from "@mui/joy/Input";
+import IconButton from "@mui/joy/IconButton";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  return <Card />
+    const [showPasswordBool, setShowPasswordBool] = useState(false);
+
+    const showPassword = () => {
+        setShowPasswordBool(!showPasswordBool);
+    };
+
+    return (
+        <main>
+            <section className="vlx-login vlx-account">
+                <div className="container">
+                    <div className="inner">
+                        <Card
+                            sx={{
+                                height: "90%",
+                                width: "90%",
+                                padding: "20px",
+                                textAlign: "center",
+                            }}
+                        >
+                            <h1>Login</h1>
+                            <h3>Email</h3>
+                            <Input type="email" />
+                            <h3>Wachtwoord</h3>
+                            <Input
+                                type={showPasswordBool ? "text" : "password"}
+                                endDecorator={
+                                    <IconButton onClick={showPassword}>
+                                        {showPasswordBool ? (
+                                            <Visibility
+                                                sx={{
+                                                    width: "25px",
+                                                    height: "25px",
+                                                }}
+                                            />
+                                        ) : (
+                                            <VisibilityOff
+                                                sx={{
+                                                    width: "25px",
+                                                    height: "25px",
+                                                }}
+                                            />
+                                        )}
+                                    </IconButton>
+                                }
+                            />
+
+                            <div className="btn-group">
+                                <Button
+                                    variant="solid"
+                                    sx={{
+                                        width: "50%",
+                                    }}
+                                >
+                                    login
+                                </Button>
+                                <Link to="/register">Registreer</Link>
+                                <Link to="/">Home</Link>
+                            </div>
+                        </Card>
+                    </div>
+                </div>
+            </section>
+        </main>
+    );
 };
 
 export default Login;
