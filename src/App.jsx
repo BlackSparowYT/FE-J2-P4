54ac2e9c79749bef8eaf949965a54001b9383a02
Jordi Verbeek <jordi.verbeek@gmail.com>
2024-06-12 14:27:11 +0200

    app.jsx import fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Routes, Route, useLocation, useNavigate } from "react-router-dom";
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
-import { Routes, Route, useLocation } from "react-router-dom";
 import firebase from "./firebase.js";
 
 // Layout
